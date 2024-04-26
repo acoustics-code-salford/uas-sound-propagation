ca4216e69d9c50ee99628157b72cbff88ab5c001
Marc Green <marc1701@gmail.com>
2024-04-26 12:05:04 +0100

    Central table with default vals

--- uasevent/main.py
+++ uasevent/main.py
@@ -1,24 +1,60 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QPushButton, QGridLayout, QWidget, QMainWindow
+from PyQt6.QtWidgets import (
+    QApplication, 
+    QPushButton, 
+    QGridLayout, 
+    QWidget, 
+    QTableWidget,
+    QTableWidgetItem,
+    QMainWindow,
+    QAbstractScrollArea,
+    QHeaderView
+)
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        topBtn = QPushButton(parent=self)
-        centerBtn = QPushButton(text="Center")
-        bottomBtn = QPushButton(text="Bottom")
+
+        self.setFixedSize(QSize(1200, 600))
         layout = QGridLayout()
-        layout.addWidget(topBtn, 0, 0, 1, 2)
-        layout.addWidget(centerBtn, 1, 0)
-        layout.addWidget(bottomBtn, 1, 1)
+
+        self.setup_flighpath_table()
+        layout.addWidget(self.flightpath_table)
         
         # set up main display
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+    def setup_flighpath_table(self):
+        self.flightpath_table = QTableWidget()
+        self.flightpath_table.setColumnCount(8)
+        self.flightpath_table.setRowCount(1)
+        self.flightpath_table.setHorizontalHeaderLabels([
+            'Start X', 
+            'Start Y', 
+            'Start Z',
+            'End X',
+            'End Y',
+            'End Z',
+            'Start Speed',
+            'End Speed'
+        ])
+        
+        # crudely set values and column widths
+        init_flightpath = [
+            '20', '-142.5', '10', '20', '142.5', '10', '15', '15'
+        ]
+        for col, val in zip(
+            range(self.flightpath_table.columnCount()), init_flightpath):
+            
+            self.flightpath_table.setColumnWidth(col, 75)
+            self.flightpath_table.setItem(0, col, QTableWidgetItem(val))
+
+        self.flightpath_table.setFixedSize(QSize(616, 100))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
