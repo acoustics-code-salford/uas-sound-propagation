84181653865b99c267c8d4b19f42769168eca7cf
Marc Green <marc1701@gmail.com>
2024-04-26 13:07:22 +0100

    Added buttons

--- uasevent/main.py
+++ uasevent/main.py
@@ -1,8 +1,11 @@
 import sys
+import json
 
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import (
     QApplication, 
+    QComboBox,
+    QFormLayout,
     QPushButton, 
     QGridLayout, 
     QWidget, 
@@ -13,15 +16,34 @@ from PyQt6.QtWidgets import (
     QHeaderView
 )
 
+from pathlib import Path
+root_path = str(Path(__file__).parent.parent)
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setFixedSize(QSize(1200, 600))
+        self.setWindowTitle('UAV Sound Propagation')
+        self.setFixedSize(QSize(700, 600))
         layout = QGridLayout()
 
+        mapping_names = list(json.load(open('mappings/mappings.json')).keys())
+        self.mapping_dropdown = QComboBox()
+        self.mapping_dropdown.addItems(mapping_names)
+        form = QFormLayout()
+        form.addRow('Loudspeaker Mapping:', self.mapping_dropdown)
+        layout.addLayout(form, 0, 0, 1, 2)
+
         self.setup_flighpath_table()
-        layout.addWidget(self.flightpath_table)
+        layout.addWidget(self.flightpath_table, 1, 0, 1, 2, 
+                         Qt.AlignmentFlag.AlignCenter)
+
+        self.add_button = QPushButton('Add Stage')
+        layout.addWidget(self.add_button, 2, 0)
+
+        self.remove_button = QPushButton('Remove Stage')
+        layout.addWidget(self.remove_button, 2, 1)
         
         # set up main display
         widget = QWidget()
@@ -49,9 +71,12 @@ class MainWindow(QMainWindow):
         ]
         for col, val in zip(
             range(self.flightpath_table.columnCount()), init_flightpath):
-            
+
+            item = QTableWidgetItem(val)
+            item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+
             self.flightpath_table.setColumnWidth(col, 75)
-            self.flightpath_table.setItem(0, col, QTableWidgetItem(val))
+            self.flightpath_table.setItem(0, col, item)
 
         self.flightpath_table.setFixedSize(QSize(616, 100))
 
